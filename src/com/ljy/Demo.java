f2589b75e27cf4cf482526ecb90a1843bf563e3a
17671798605 <971785472@qq.com>
2019-11-21 18:49:44 +0800

    初始

---
+++ src/com/ljy/Demo.java
@@ -0,0 +1,13 @@
+package com.ljy;
+
+/**
+ * @author xyzhang
+ * @ClassName Demo
+ * @description
+ * @date 2019/11/21 0021
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
